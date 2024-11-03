9acc3cead083dd0de313857b9e7c641dbac87347
Andrii <m9ta92@gmail.com>
2024-11-03 17:01:18 +0100

    Update RegistrationPage.jsx

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,4 +1,4 @@
-import { RegistrationForm } from '/Projects/goit/react/goit-react-hw-08/src/components/RegistrationForm/RegistrationForm';
+import { RegistrationForm } from '../../components/RegistrationForm/RegistrationForm';
 
 export default function RegisterPage() {
 	return (
